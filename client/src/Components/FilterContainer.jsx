0c518ff531d2d10d272eda25e323d88f8d1b8f05
Emily <emilyhigginson14@gmail.com>
2022-04-20 10:54:24 -0400

    favorite fix

--- client/src/Components/FilterContainer.jsx
+++ client/src/Components/FilterContainer.jsx
@@ -15,7 +15,7 @@ function FilterContainer({ spots, user }) {
   const filteredSpots = spots.map(spot =>
     <SpotCard
       reviews={spot.reviews}
-      key={spot.id} spot_id={spot.id} name={spot.name} user={user} location={spot.location} category={spot.category} lat={spot.lat} lng={spot.lng} image={spot.image} />)
+      key={spot.id} id={spot.id} name={spot.name} user={user} location={spot.location} category={spot.category} lat={spot.lat} lng={spot.lng} image={spot.image} />)
 
   return (
     <div id='filterContainer'>
